4250ce69b309647b7247b27e9335b8fbb3b15ecc
Chelsea Otakan <chexee@gmail.com>
2016-02-26 00:47:07 -0800

    Update portfolio.

---
+++ scripts/_emoji.js
@@ -0,0 +1,12 @@
+var emoji = new Object()
+
+emoji.toEmoji = function(element) {
+  return element.innerHTML = emojione.toImage(element.innerHTML)
+}
+
+emoji.init = function() {
+  for ( var i = 0; i < document.getElementsByClassName('emoji').length; i++) {
+    emoji.toEmoji(document.getElementsByClassName('emoji')[i])
+    console.log( emoji.toEmoji(document.getElementsByClassName('emoji')[i]) )
+  }
+}
